476ba8d9d3c304536122bf515933600f9590675a
springbottom <joshua.z.lin@gmail.com>
2016-11-05 00:43:59 -0700

    added facebook function

--- background.js
+++ background.js
@@ -10,3 +10,12 @@ document.onkeypress = function (e) {
     console.log("suces");
     // use e.keyCode
 };
+
+function bad1(){
+	//This is like what happens if you idle
+	//hopefully the facebook one.
+
+	alert('yaad')
+
+
+}
