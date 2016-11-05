24e345f941f5f2d934d73b9739acc63b2c5ec07d
Jason Lum <jthemathfreak@gmail.com>
2016-11-04 20:03:53 -0700

    key event listener doesnt work

--- content.js
+++ content.js
@@ -1,8 +1,11 @@
-
 var handler = function (e) { 
     handler.data.push(e);
+    console.log("success")
     console.log(handler.data);
 }
 handler.data = [];
 console.log("background running")
+console.log("testingsdfakj")
 window.addEventListener("keydown", handler)
+document.addEventListener("keydown", handler)
+console.log("Here")
