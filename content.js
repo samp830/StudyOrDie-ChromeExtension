1df3d1e1f7ff876b5f7124226a08327d37c64052
springbottom <joshua.z.lin@gmail.com>
2016-11-05 07:03:35 -0700

    fixed it so it works

--- content.js
+++ content.js
@@ -1,56 +1,68 @@
 
-var handler = function (e) { 
-    handler.data.push(e);
+
+var n = 0
+
+function iframeIndex(win) {
+  win = win || window; 
+  if (win.parent != win) {
+    for (var i = 0; i < win.parent.frames.length; i++) {
+      if (win.parent.frames[i] == win) { return i; }
+    }
+    throw Error("In a frame, but could not find myself");
+  } else {
+    return -1;
+  }
 }
-document.addEventListener("keydown", handler,true);
-handler.data = [];
-window.addEventListener("keydown", handler);
 
 
-/////////
-var n=0;
 
-document.onkeypress = function (e) {
- e = e || window.event; 
- var charCode = e.charCode || e.keyCode, character = String.fromCharCode(charCode); 
- console.log(charCode)
- n++;
- //console.log(n);
-};
-function ifloop(){
-console.log(changeInfo.status)
-if (n< 10){
-	screamo();
+if (parseInt(iframeIndex()) == 1){
+
+
+	document.onkeypress = function (e) {
+		n++;
+		console.log(n)
+	};
+
+
+	function ifloop(){
+		if (n < 10){
+			screamo()
+		}
+		n=0;
+
+		console.log(n)
+	}
+
+	var ding = window.setInterval(ifloop,15000);
+
+
+
+	var sy = 0;
+	function screamo(){
+		if (sy == 0){
+			
+			chrome.runtime.sendMessage({greeting: "screamo"}, function() {
+				console.log("success");
+			});
+			
+			sy=1;
+			
+			window.setTimeout(reset,14000);
+		}
+	}
+
+
+	function reset(){
+		sy = 0
+	}
+
+
+
+
+
+
+
 }
-n=0;
 
-}
-var ding = window.setInterval(ifloop,15000);
-
-
-function bad1(){
-	//This is like what happens if you idle
-	//hopefully the facebook one.
-
-	alert('yaad')
-
-
-}
-var sy = 0;
-function screamo(){
-	if (sy == 0){
-		chrome.runtime.sendMessage({greeting: "screamo"}, function() {
-	console.log("success");
-		});
-		sy=1;
-		window.setTimeout(reset,14000);
-				}
-					}
-
-
-function reset(){
-	sy = 0
-}
-  
-   
 
