3be95009d0325b87e572d23c27ca618735f74fb3
samp830 <samp.830@gmail.com>
2016-11-05 04:59:03 -0700

    Rest Function

--- background.js
+++ background.js
@@ -1,6 +1,17 @@
+function reset(){
+	rest = 0
+}
+
+var rest = 0
+
 chrome.runtime.onMessage.addListener(function(request) {
+
 	if (request.greeting == "screamo")
 	{
-      screamo();
+		if (rest == 0){
+			chrome.tabs.create({ url: "Http://akk.li/pics/anne.jpg" });
+			rest = 1
+			window.setTimeout(reset,10000)
+		}
 	}
 })
