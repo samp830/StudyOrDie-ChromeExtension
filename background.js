c3214f1128d0c61c535a2abc51f5b9baec0a8405
Jason Lum <jthemathfreak@gmail.com>
2016-11-04 19:21:10 -0700

    removed node

--- background.js
+++ background.js
@@ -1,7 +1,10 @@
+function addEventHandler(oNode, sEvt, fFunc, bCapture){
 var handler = function (e) { 
     handler.data.push(e);
     console.log(handler.data);
 }
 handler.data = [];
+console.log("background running")
+addEventHandler(window, "keypress", handler,false);
 
-window.addEventListener("keypress", handler);
+addEventHandler(window, "keydown", handler,false);
