974358bf72aa32d2ecd50f084e5bd4a3048b6615
springbottom <joshua.z.lin@gmail.com>
2016-11-05 07:55:00 -0700

    added facebook punishment reroute

--- background.js
+++ background.js
@@ -3,6 +3,10 @@
 chrome.runtime.onMessage.addListener(function(request) {
 	if (request.greeting == "screamo")
 	{
-		chrome.tabs.create({ url: "Http://akk.li/pics/anne.jpg" });
+		chrome.tabs.create({ url: "https://static.pexels.com/photos/27714/pexels-photo-27714-large.jpg" });
+	}
+	if (request.greeting == "faceo")
+	{
+		chrome.tabs.create({ url: "https://www.facebook.com/messages"})
 	}
 })
