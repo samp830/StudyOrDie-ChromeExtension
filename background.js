4d77fa889cc2a35b9b10d78c033fdb973b61826b
samp830 <samp.830@gmail.com>
2016-11-05 02:04:38 -0700

    Removed same functions

--- background.js
+++ background.js
@@ -1,32 +1,3 @@
-
-var handler = function (e) { 
-    handler.data.push(e);
-    console.log(handler.data);
-}
-handler.data = [];
-console.log("background running");
-window.addEventListener("keydown", handler);
-document.onkeypress = function (e) {
-    
- e = e || window.event; 
- var charCode = e.charCode || e.keyCode, character = String.fromCharCode(charCode); 
- console.log(charCode)
- 
-};	
-
-function bad1(){
-	//This is like what happens if you idle
-	//hopefully the facebook one.
-
-	alert('yaad')
-
-
-}
-function screamo(){
-chrome.tabs.create({ url: "Http://akk.li/pics/anne.jpg" })
-console.log("why")
-}
-
 chrome.runtime.onMessage.addListener(function(request) {
 	if (request.greeting == "screamo")
 	{
