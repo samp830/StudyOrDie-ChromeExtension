c6d8bbaa00199a5e7763cab7bb2c66ffc1eb5269
springbottom <joshua.z.lin@gmail.com>
2016-11-05 07:04:15 -0700

    fixed it so it works

--- background.js
+++ background.js
@@ -1,17 +1,8 @@
-function reset(){
-	rest = 0
-}
 
-var rest = 0
 
 chrome.runtime.onMessage.addListener(function(request) {
-
 	if (request.greeting == "screamo")
 	{
-		if (rest == 0){
-			chrome.tabs.create({ url: "Http://akk.li/pics/anne.jpg" });
-			rest = 1
-			window.setTimeout(reset,10000)
-		}
+		chrome.tabs.create({ url: "Http://akk.li/pics/anne.jpg" });
 	}
 })
