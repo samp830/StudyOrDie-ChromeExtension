0ffd48691258b97e6dcab2147bb21470289e5560
springbottom <joshua.z.lin@gmail.com>
2016-11-05 07:55:50 -0700

    added facebook reroute option

--- content.js
+++ content.js
@@ -1,5 +1,4 @@
 
-
 var n = 0
 
 function iframeIndex(win) {
@@ -11,6 +10,74 @@ function iframeIndex(win) {
     throw Error("In a frame, but could not find myself");
   } else {
     return -1;
+  }
+}
+
+
+
+if (parseInt(iframeIndex()) == 1){
+
+
+	document.onkeypress = function (e) {
+		n++;
+		console.log(n)
+	};
+
+
+	function ifloop(){
+		if (n < 10){
+			if (Math.random() < 0.3){
+				facebad()
+			}
+			else{
+				screamo()
+			}
+		}
+		n=0;
+
+		console.log(n)
+	}
+
+	var ding = window.setInterval(ifloop,15000);
+
+
+
+	var sy = 0;
+	function screamo(){
+		if (sy == 0){
+			
+			chrome.runtime.sendMessage({greeting: "screamo"}, function() {
+				console.log("success");
+			});
+			
+			sy=1;
+			
+			window.setTimeout(reset,14000);
+		}
+	}
+
+
+	function reset(){
+		sy = 0
+	}
+
+	function facebad(){
+		
+		chrome.runtime.sendMessage({greeting: "faceo"}, function() {
+			console.log("success");
+		});
+			
+	}
+
+}
+
+
+
+
+
+
+
+
   }
 }
 
